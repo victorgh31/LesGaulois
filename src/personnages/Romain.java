3a6939f9dd03a5349d41aa67ba467d15f19c542a
victorgh31 <victor.bertrand31@gmail.com>
2023-10-02 23:34:54 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,6 +4,8 @@ public class Romain {
 
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -40,11 +42,41 @@ public class Romain {
 		}
 	}
 	
-		public static void main(String[] args) {
-			Romain minus;
-			
-			minus = new Romain("Minus", 6);
-			System.out.println(minus.getNom());
-			minus.recevoirCoup(5);
-		}
+	 public void sEquiper(Equipement equipement) {
+        switch (nbEquipement) {
+            case 0:
+                ajouterEquipement(equipement);
+                break;
+            case 1:
+                if (equipements[0] == equipement) {
+                    System.out.println("Le soldat " + getNom() + " possède déjà un " + equipement.getNom() + " !");
+                } else {
+                    ajouterEquipement(equipement);
+                }
+                break;
+            case 2:
+                System.out.println("Le soldat " + getNom() + " est déjà bien protégé !");
+                break;
+            default:
+                System.out.println("Le soldat " + getNom() + " a un nombre d'équipements non valide.");
+        }
+	 }
+
+	 private void ajouterEquipement(Equipement equipement) {
+		 equipements[nbEquipement] = equipement;
+         nbEquipement++;
+         System.out.println("Le soldat " + getNom() + " s'équipe avec un " + equipement.getNom() + ".");
+     }
+	
+	 public static void main(String[] args) {
+		 Romain minus = new Romain("Minus", 6);
+		// Le soldat Minus s'équipe avec un casque.
+		 minus.sEquiper(Equipement.CASQUE);
+		// Le soldat Minus possède déjà un casque.
+         minus.sEquiper(Equipement.CASQUE);
+         // Le soldat Minus s'équipe avec un bouclier.
+         minus.sEquiper(Equipement.BOUCLIER);
+         // Le soldat Minus est déjà bien protégé !
+         minus.sEquiper(Equipement.CASQUE);
+	}
 }
