c7638d080088f14f1b2cb7a6edba135d8deb390b
victorgh31 <victor.bertrand31@gmail.com>
2023-10-03 17:47:24 +0200

    Prochaine étape : utilisation du debugger !

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -1,24 +1,66 @@
 package histoire;
 
 import personnages.Druide;
+import personnages.Equipement;
 import personnages.Gaulois;
 import personnages.Romain;
+import villagegaulois.Musee;
 
 public class Scenario {
 
 	public static void main(String[] args) {
-		Druide panoramix = new Druide("Panoramix", 5, 10);
+		Druide druide = new Druide("Panoramix", 5, 10);
+		druide.parler("Je vais aller préparer une petite potion...");
+		druide.preparerPotion();
 		Gaulois obelix = new Gaulois("Obélix", 25);
 		Gaulois asterix = new Gaulois("Astérix", 8);
-		Romain minus = new Romain("Minus", 6);
-		
-		panoramix.preparerPotion(10);
-		panoramix.booster(obelix);
+		druide.booster(obelix);
 		obelix.parler("Par Bélénos, ce n'est pas juste !");
-		panoramix.booster(asterix);
+		druide.booster(asterix);
 		asterix.parler("Bonjour");
+		Romain minus = new Romain("Minus", 6);
+		Romain milexcus = new Romain("Milexcus", 8);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		milexcus.sEquiper(Equipement.CASQUE);
 		minus.parler("UN GAU... UN GAUGAU...");
-		asterix.frapper(minus);
+		do {
+			asterix.frapper(minus);
+		} while (minus.getForce() > 0);
+		milexcus.parler("UN GAU... UN GAUGAU...");
+		do {
+			asterix.frapper(milexcus);
+		} while (milexcus.getForce() > 0);
+		
+//		Partie a decommenter
+		
+		Musee musee = new Musee();
+		asterix.faireUneDonnation(musee);
+
 	}
-	
+
 }
+//package histoire;
+//
+//import personnages.Druide;
+//import personnages.Gaulois;
+//import personnages.Romain;
+//
+//public class Scenario {
+//
+//	public static void main(String[] args) {
+//		Druide panoramix = new Druide("Panoramix", 5, 10);
+//		Gaulois obelix = new Gaulois("Obélix", 25);
+//		Gaulois asterix = new Gaulois("Astérix", 8);
+//		Romain minus = new Romain("Minus", 6);
+//		
+//		panoramix.preparerPotion(10);
+//		panoramix.booster(obelix);
+//		obelix.parler("Par Bélénos, ce n'est pas juste !");
+//		panoramix.booster(asterix);
+//		asterix.parler("Bonjour");
+//		minus.parler("UN GAU... UN GAUGAU...");
+//		asterix.frapper(minus);
+//	}
+//	
+//}
